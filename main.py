6640018c25bc169f86865fb9585d77113954f7dc
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-09-17 10:47:05 +0900

    split large page

--- main.py
+++ main.py
@@ -267,26 +267,41 @@ def make_scrapbox_json(directory):
         print(f"Skip it because not uploaded all images.")
         return
 
+    # Concatenate images and the OCR texts into a scrapbox page.
+    # Due to the Scrapbox accepts only 10000 lines per page,
+    # we need to split the pages.
+    scrapbox_pages = []
+    page_count = 1
+    def add_page(page_lines):
+        nonlocal page_count
+        new_title = title
+        if page_count > 1:
+            new_title += f" ({page_count})"
+        page_lines[0] = new_title
+        scrapbox_pages.append({
+            "title": new_title,
+            "lines": page_lines
+        })
+        page_count += 1
 
     page_lines = [title, f"local_path: {directory}", ""]
 
     for page in gyazo_info:
-        # "permalink_url"
         page_lines.append(f"[{page['permalink_url']}]")
         if "ocr_text" in page:
             page_lines.extend(page["ocr_text"].split("\n"))
         page_lines.append("")
+        if len(page_lines) > 9000:
+            add_page(page_lines)
+            page_lines = [title, f"local_path: {directory}", ""]
 
     # Make the JSON
-    scrapbox_json = {"pages": [{
-        "title": title,
-        "lines": page_lines
-    }]}
+    scrapbox_json = {"pages": scrapbox_pages}
 
     # Save the JSON
     out_path = os.path.join(directory, "scrapbox.json")
     with open(out_path, 'w') as f:
-        json.dump(scrapbox_json, f, indent=2)
+        json.dump(scrapbox_json, f, indent=2, ensure_ascii=False)
 
 
 def get_pdfs_in_dir():
